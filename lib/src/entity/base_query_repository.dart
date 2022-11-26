e594a6d551e2a4b9ce944bfb83cc335aaad37d10
mohamed ali <mohamed@ali.com>
2022-11-26 18:09:39 +0200

    dbcontext injectable into repository constructor not function parameter

--- lib/src/entity/base_query_repository.dart
+++ lib/src/entity/base_query_repository.dart
@@ -3,17 +3,18 @@ import '../../dart_data_source.dart';
 abstract class BaseQueryRepository<T extends BaseEntity> {
   late final DbTable querable;
   late final AbsDatabase db;
+  late final DbContext dbc;
 
-  BaseQueryRepository(this.querable){
+  BaseQueryRepository(this.querable, this.dbc){
     this.db = querable.db;
   }
 
-  Future<List<T>> query(Expr cond, T Function() entityCreator, DbContext dbc) async {
+  Future<List<T>> query(Expr cond, T Function() entityCreator) async {
     var list = await querable.db.Select().From(querable).Where(cond).execute(dbc);
     return list.map<T>((e) => entityCreator()..fromMap(e)).toList();
   }
 
-  Future<List<T>> all(T Function() entityCreator, DbContext dbc) async {
+  Future<List<T>> all(T Function() entityCreator) async {
     var list = await querable.db.Select().From(querable).execute(dbc);
     return list.map<T>((e) => entityCreator()..fromMap(e)).toList();
   }
